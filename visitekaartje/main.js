0a413b08e334eb9ae5681d8a4daed2600909a1ef
DennisHvA <dennis.de.graaf2@hva.nl>
2023-02-07 15:06:44 +0100

    base card with all js and most styling

--- visitekaartje/main.js
+++
@@ -1,3 +0,0 @@
-fetch('https://cors-anywhere.herokuapp.com/https://whois.fdnd.nl/api/v1/member?id=cldepc8ku3x1b0av0dgvl1pmw')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
---
+++ visitekaartje/scripts/main.js
@@ -0,0 +1,29 @@
+fetch('./data.json')
+
+// fetch('https://cors-anywhere.herokuapp.com/https://whois.fdnd.nl/api/v1/member?id=cldepc8ku3x1b0av0dgvl1pmw')
+    .then(response => response.json())
+    .then(data => {
+
+        const fullname = []
+        fullname.push(data.member.name + " " + data.member.prefix + " " + data.member.surname)
+        document.querySelector("#name").innerHTML = fullname
+
+        const nickname = []
+        nickname.push(data.member.nickname)
+        document.querySelector("#nickname").innerHTML = nickname
+
+        const avatar = []
+        avatar.push(data.member.avatar)
+        const avatarid = document.querySelector("#avatar")
+        avatarid.src = avatar
+
+        const bio = []
+        bio.push(data.member.bio)
+        document.querySelector("#bio").innerHTML = bio
+
+        const githubid = document.querySelector("#github")
+        githubid.href = data.member.gitHubHandle
+
+        const websiteid = document.querySelector("#website")
+        websiteid.href = data.member.website
+    })
